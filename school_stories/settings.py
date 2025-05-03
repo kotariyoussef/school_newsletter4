a7915c1529c0d2be7af93876b0d64f9aa85447e4
Your Name <you@example.com>
2025-05-03 12:33:41 +0200

    Your commit message here2

--- school_stories/settings.py
+++ school_stories/settings.py
@@ -25,11 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-6vayrb7qk26m@&ze^jm05h^#s$c%$$p1zv46m1h_lm%rvw)y%@')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = os.getenv('DEBUG') == 'True'
 
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
